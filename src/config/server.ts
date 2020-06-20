cc0bcb9868ba23dfadb80a9cdc9a43ca2d3fd9af
Leonardo Viana <leonardo.viana@meep-app.com>
2020-06-20 20:10:01 -0300

    Deploy aplication

---
+++ src/config/server.ts
@@ -0,0 +1,6 @@
+import app from '../app';
+import {connect} from '../database/mongoose';
+
+connect();
+
+app.listen(process.env.PORT || 3000);
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import * as mongoose from 'mongoose';
-import {connect} from './database/mongoose';
-
-connect();
-
-app.listen(process.env.PORT || 3000);
